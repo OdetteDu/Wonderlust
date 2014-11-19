7841cb6c1df1fcae3ccdbbf857ef8020309fdc88
Gabe Poon <Poon@dn51vcl0.sunet>
2014-11-18 22:25:23 -0800

    Added app and website folders

---
+++ app/Wonder/FavoriteViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoriteViewController.h
+//  Wonder
+//
+//  Created by Caidie on 14-11-18.
+//  Copyright (c) 2014年 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoriteViewController : UIViewController
+
+@end
