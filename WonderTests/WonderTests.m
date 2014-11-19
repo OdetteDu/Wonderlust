f42e7ef5860d839616962b32a08da95a1b8b4a08
Gabe Poon <Poon@dn51vcl0.sunet>
2014-11-18 22:07:36 -0800

    Updated project

---
+++ WonderTests/WonderTests.m
@@ -0,0 +1,34 @@
+//
+//  WonderTests.m
+//  WonderTests
+//
+//  Created by Caidie on 14-11-18.
+//  Copyright (c) 2014年 Stanford. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WonderTests : XCTestCase
+
+@end
+
+@implementation WonderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
